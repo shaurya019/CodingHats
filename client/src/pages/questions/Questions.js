1596398aac2ddb517de2c3a101472b0210caf151
shaurya019 <shauryarana2000@gmail.com>
2021-10-29 23:57:35 +0530

    hi

--- client/src/pages/questions/Questions.js
+++ client/src/pages/questions/Questions.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
-
+import "./questions.css";
 export default class Questions extends Component {
   render() {
-    return <div>Questions</div>;
+    return <div className="section-1">
+    <div className="container">
+      <div className="row">
+
+
+      <div className="Part-1">
+      <div className="question-main-head">
+        <h2>Weekly Question</h2>
+        </div>
+
+      <div className="question-head">
+      <h3>Try to solve the question!!</h3>
+        
+
+        <div className="question-box">
+        <p>Question</p>
+        <div className="question-sol">
+        <a id="link1" href="" target="_blank">Text Solution</a>
+        <a id="link2" href="" target="_blank">Video Solution</a>
+        </div>
+        </div>
+        </div>
+        </div>
+
+
+
+        <div className="Part-2">
+        
+        </div>
+      </div>
+      </div>
+      </div>;
   }
 }
