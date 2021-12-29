20feea98b06ec5b7bf23f5a2758349a37fdee459
shaurya019 <shauryarana2000@gmail.com>
2021-12-29 13:17:14 +0530

    v

--- client/src/pages/projects/Projects.js
+++ client/src/pages/projects/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./projects.css";
 import { changeSelect } from "../../context/sidebarContext/SidebarAction";
 class Projects extends React.Component {
   componentDidMount() {
@@ -6,7 +7,49 @@ class Projects extends React.Component {
     SidebarDispatch(changeSelect("projects"));
   }
   render() {
-    return <div>Projects</div>;
+    return (
+      <div className="Projects-section">
+      <div className="Project-col">
+      <div className="Project-head">
+      <div id="Project-head-1">
+        <h2>Projects</h2>
+        </div> 
+        <div id="Project-head-2">
+        <h4>Cool Intresting Stuff!!</h4>
+        </div> 
+        </div> 
+        <hr></hr>
+      <div className="row">
+      <div className="Project-setup">
+      <div className="Project-set-1">
+        <div className ="set-box" id="p-box-1">
+          <h5>Basic website</h5>
+        </div>
+        <div className ="set-box" id="p-box-2">
+          <h5>JavaScript</h5>
+        </div>
+        </div>
+        <div className="Project-set-2">
+        <div className ="set-box" id="p-box-3">
+          <h5>React</h5>
+        </div>
+        <div className ="set-box" id="p-box-4">
+          <h5>Android</h5>
+        </div>
+      </div> 
+      <div className="Project-set-3">
+        <div className ="set-box" id="p-box-5">
+          <h5>Python</h5>
+        </div>
+        <div className ="set-box" id="p-box-6">
+          <h5>Machine Learning</h5>
+        </div>
+      </div> 
+      </div> 
+      </div>  
+      </div>  
+      </div>
+    );
   }
 }
 
