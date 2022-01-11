2d342212aea92fa501bd6319871f1cf1557725e6
shubhamgarg21156 <gargshubham21156@gmail.com>
2022-01-11 22:11:41 +0530

    hi

--- admin/src/pages/dayVise/DayVise.js
+++ admin/src/pages/dayVise/DayVise.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import { changeSelect } from "../../context/SidebarContext/SidebarAction";
-import {useEffect , useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './DayVise.css';
 
 function DayVise(props) {
-    useEffect(() => {
-        props.SidebarDispatch(changeSelect("dayViseQuestion"));
-      }, [props.SidebarDispatch]);
-    
-    const [list, changeList] = useState([]);
-    
-    const [dayQuestion, setQuestion] = useState({});
+  useEffect(() => {
+    props.SidebarDispatch(changeSelect("dayViseQuestion"));
+  }, [props.SidebarDispatch]);
 
-    useEffect(() => {
-      const fetchQuestions = async () => {
-        const res = await axios.get("/question");
-        changeList(res.data);
-      };
-      fetchQuestions();
-    }, []);
+  const [list, changeList] = useState([]);
 
-    const handleSelect = (e) => {
-      let value = Array.from(e.target.selectedOptions , (option) => option.value);
-      setQuestion({...dayQuestion , [e.target.name] : value})
-    }
-  
+  const [dayQuestion, setQuestion] = useState({});
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const res = await axios.get("/question");
+      changeList(res.data);
+    };
+    fetchQuestions();
+  }, []);
+
+  const handleSelect = (e) => {
+    let value = Array.from(e.target.selectedOptions, (option) => option.value);
+    setQuestion({ ...dayQuestion, [e.target.name]: value })
+  }
+
 
   const handleChange = (e) => {
     setQuestion({ ...dayQuestion, [e.target.name]: e.target.value });
@@ -44,32 +45,32 @@ function DayVise(props) {
     }
   };
 
-    return (
-        <div>
-            <div className="Question">
-                <div className="QuestionsDay">
-                <div className="QuestionsDayTitle">Day</div>
-                <div className="QuestionsDayTitleInput">
-                    <input onChange={handleChange} name="day"></input>
-              </div>
-            </div>
-
-          <div className="addQuestions">
-              <select multiple name="questionsArray" onChange={handleSelect}>
-                  {list.map((question) => (
-                    <option key={question._id} value={question._id}>
-                        {question.title} 
-                    </option>
-                  ))
-                  }
-              </select>
+  return (
+    <div>
+      <div className="Question">
+        <div className="QuestionsDay">
+          <div className="QuestionsDayTitle">Day</div>
+          <div className="QuestionsDayTitleInput">
+            <input onChange={handleChange} name="day"></input>
           </div>
+        </div>
+
+        <div className="addQuestions">
+          <select multiple name="questionsArray" onChange={handleSelect}>
+            {list.map((question) => (
+              <option key={question._id} value={question._id}>
+                {question.title}
+              </option>
+            ))
+            }
+          </select>
+        </div>
         <button className="addQuestionButton" onClick={handleSubmit}>
           Add this day Questions
         </button>
       </div>
-      </div>
-    )
+    </div>
+  )
 }
 
 export default DayVise
